5524fb6be2cd6aedb304b68ba0bdad3f2b7817bf
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-19 22:42:50 -0300

    Adição da classe controladora UsuarioController e da entidade Usuario.

--- src/main/java/org/SpringBoot_GitHub/Controllers/UsuarioController.java
+++ src/main/java/org/SpringBoot_GitHub/Controllers/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-public class Controlador {
+public class UsuarioController {
 
     @GetMapping("/saludo")
     public String olá() {
