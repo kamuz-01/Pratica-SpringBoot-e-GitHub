71ddd738fa709e1c0c54faada9396144e5eacb00
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 12:18:36 -0300

    DOCS: Adição de comentários na interface UsuarioRepository

--- src/main/java/org/SpringBoot_GitHub/Repositories/UsuarioRepository.java
+++ src/main/java/org/SpringBoot_GitHub/Repositories/UsuarioRepository.java
@@ -3,4 +3,6 @@ package org.SpringBoot_GitHub.Repositories;
 import org.SpringBoot_GitHub.Models.Entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/* O repositório pede dois parâmetros: a entidade que representa o 
+ * domínio e o tipo do ID do domínio */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
