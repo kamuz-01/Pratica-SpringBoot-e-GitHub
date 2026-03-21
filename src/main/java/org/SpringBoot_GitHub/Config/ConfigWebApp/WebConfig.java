f8e6267c51154fcf07454c931ca4118c58447897
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 19:33:47 -0300

    FEAT: Implementa a classe WebConfig

--- src/main/java/org/SpringBoot_GitHub/Config/ConfigWebApp/WebConfig.java
+++ src/main/java/org/SpringBoot_GitHub/Config/ConfigWebApp/WebConfig.java
@@ -1,7 +1,21 @@
 package org.SpringBoot_GitHub.Config.ConfigWebApp;
 
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.SpringBoot_GitHub.Loggings.InterceptorLoggingApiUsuario;
 
+@Configuration
 public class WebConfig  implements WebMvcConfigurer {
 
+    private final InterceptorLoggingApiUsuario interceptorLoggingApiUsuarios;
+
+    public WebConfig(InterceptorLoggingApiUsuario interceptorLoggingApiUsuarios) {
+		this.interceptorLoggingApiUsuarios = interceptorLoggingApiUsuarios;
+	}
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptorLoggingApiUsuarios);
+    }
 }
