e131d56f7a6d74d0b31a3590ef904c7802a1f08b
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-20 21:37:36 -0300

    Adição da classe (Entidade) Missoes

---
+++ src/main/java/org/SpringBoot_GitHub/Models/Entities/Missoes.java
@@ -0,0 +1,47 @@
+package org.SpringBoot_GitHub.Models.Entities;
+
+import org.SpringBoot_GitHub.Models.Enums.GrauDificuldade;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "missoes")
+public class Missoes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_missao")
+    private Long idMissao;
+
+    @Column(name = "nome_missao", nullable = false)
+    private String nomeMissao;
+
+    @Column(name = "descricao", nullable = false)
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "grau_dificuldade", nullable = false)
+    private GrauDificuldade grauDificuldade;
+
+    // Relacionamento @OneToMany - Uma missão pode ser feita por vários usuários
+    @OneToMany(mappedBy = "missoes")
+    @JoinColumn(name = "id_usuario", nullable = false) // Foreign Key
+    private Usuario usuario;
+}
