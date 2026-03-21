19111d563a6ca5083134eba1f29e90ad21474ba7
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 17:46:46 -0300

    FEAT: Implementação da classe InterceptorLoggingAPIUsuario

--- src/main/java/org/SpringBoot_GitHub/Loggings/InterceptorLoggingApiUsuario.java
+++ src/main/java/org/SpringBoot_GitHub/Loggings/InterceptorLoggingApiUsuario.java
@@ -1,7 +1,44 @@
 package org.SpringBoot_GitHub.Loggings;
 
 import org.springframework.web.servlet.HandlerInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class InterceptorLoggingApiUsuario implements HandlerInterceptor{
 
+    	private static final Logger accessLog =
+            LoggerFactory.getLogger("API_ACCESS_LOG");
+
+    private static final String START_TIME = "startTime";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) {
+
+        request.setAttribute(START_TIME, System.currentTimeMillis());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex) {
+
+        long startTime = (long) request.getAttribute(START_TIME);
+        long duration = System.currentTimeMillis() - startTime;
+
+        String ip = request.getRemoteAddr();
+        String method = request.getMethod();
+        String uri = request.getRequestURI();
+        int status = response.getStatus();
+
+        accessLog.info(
+                "IP={} METHOD={} URI={} STATUS={} TIME={}ms",
+                ip, method, uri, status, duration
+        );
+    }
 }
