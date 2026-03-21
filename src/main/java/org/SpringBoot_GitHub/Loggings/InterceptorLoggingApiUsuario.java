ec85963ba5fb1fd2279105228fee0d0621867531
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 19:36:41 -0300

    FIX: Adição do import da annotation @Component

--- src/main/java/org/SpringBoot_GitHub/Loggings/InterceptorLoggingApiUsuario.java
+++ src/main/java/org/SpringBoot_GitHub/Loggings/InterceptorLoggingApiUsuario.java
@@ -5,7 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class InterceptorLoggingApiUsuario implements HandlerInterceptor{
 
     	private static final Logger accessLog =
