31f5c84d122872cb960e7ba3e5557146e13684ad
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 12:24:39 -0300

    FEAT: Adição do Interceptador de loggings

---
+++ src/main/java/org/SpringBoot_GitHub/Loggings/InterceptorLoggingApiUsuario.java
@@ -0,0 +1,7 @@
+package org.SpringBoot_GitHub.Loggings;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class InterceptorLoggingApiUsuario implements HandlerInterceptor{
+
+}
