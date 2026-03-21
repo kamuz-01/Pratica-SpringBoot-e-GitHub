c1d15809367a2b15ac3f544cbea1e7edfe045a04
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 12:49:01 -0300

    FIX: Adição da anotação @JoinColumn

--- src/main/java/org/SpringBoot_GitHub/Models/Entities/Usuario.java
+++ src/main/java/org/SpringBoot_GitHub/Models/Entities/Usuario.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -48,5 +49,6 @@ public class Usuario {
 
     // Relacionamento @ManyToOne - Um usuário pode ter uma única missão
     @ManyToOne
-    private List<Missoes> missoes;
+    @JoinColumn(name = "id_missao", nullable = false) // Foreign Key
+    private Missoes missoes;
 }
