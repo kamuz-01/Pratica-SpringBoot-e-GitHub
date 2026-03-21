eb1eacb38990349763a0f8ea000e08f197e34885
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 13:33:48 -0300

    FIX: Implementação da classe UsuarioDTO

--- src/main/java/org/SpringBoot_GitHub/Models/DTOs/UsuarioDTO.java
+++ src/main/java/org/SpringBoot_GitHub/Models/DTOs/UsuarioDTO.java
@@ -1,5 +1,27 @@
 package org.SpringBoot_GitHub.Models.DTOs;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
 
+    private Long idUsuario;
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String telefone;
+    private String email;
+
+    @Column(name = "id_missao", nullable = false)
+    private Long idMissao;
 }
