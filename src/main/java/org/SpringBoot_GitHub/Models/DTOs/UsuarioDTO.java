38f156ec80d856946f35453391e16b062dad773a
Karli Munoz <146567084+kamuz-01@users.noreply.github.com>
2026-03-21 20:18:22 -0300

    FIX: Adciona validação aos campos do usuário.

--- src/main/java/org/SpringBoot_GitHub/Models/DTOs/UsuarioDTO.java
+++ src/main/java/org/SpringBoot_GitHub/Models/DTOs/UsuarioDTO.java
@@ -1,7 +1,10 @@
 package org.SpringBoot_GitHub.Models.DTOs;
 
 import java.time.LocalDate;
-
+import org.hibernate.validator.constraints.br.CPF;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,13 +18,28 @@ import lombok.Setter;
 public class UsuarioDTO {
 
     private Long idUsuario;
+
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "O sobrenome é obrigatório")
     private String sobrenome;
+
+    @NotBlank(message = "O CPF é obrigatório")
+    @CPF(message = "Formato de CPF inválido")
     private String cpf;
+
+    @NotNull(message = "A data de nascimento é obrigatória")
     private LocalDate dataNascimento;
+
+    @NotBlank(message = "O telefone é obrigatório")
     private String telefone;
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
     private String email;
 
     @Column(name = "id_missao", nullable = false)
+    @NotNull(message = "A missão vinculada é obrigatória")
     private Long idMissao;
 }
